384eae07b4edf6e3ef0dae4bb3da8af6b0e0bbc9
Roxana Padila <mjrubioh@gmail.com>
2018-05-22 13:29:22 -0400

    se agrego otro campo relacional con los horarios de los estdiantes.

--- sugerencias/model/suge.py
+++ sugerencias/model/suge.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-
-
 from odoo import api, fields, models, _
+from odoo.exceptions import UserError
 
 
 
@@ -11,33 +10,35 @@ from odoo import api, fields, models, _
 class sugerecla(models.Model):
     _name = "suge.recla"
     
+    name = fields.Char(string='Nombre:', required=True, readonly=False,)
+    apell = fields.Char(string='Apellido:', required=True, readonly=False,)
+    ced = fields.Char(string='Cédula:', required=True, readonly=False, )
+    corr = fields.Char(string='Correo:', required=True, readonly=False,)
+    tlf = fields.Char(string='Teléfono:', required=True, readonly=False,)
+    sex = fields.Selection([
+        ('mas', 'Masculino'),
+        ('fem', 'Femenino'),
+        ('ind', 'Indefinido'),
+        ], string='Sexo:', compute='_compute_invoice_status', store=True, readonly=False)
+    semes = fields.Char(string='Semestre:', required=True, readonly=False,)
+    carr = fields.Char(string='Carrera:', required=True, readonly=False,)
+    horario = fields.Many2one('clase.horario', string='Horario', required=True)
+    suge = fields.Text(string='Sugerencias o Reclamos:', required=True, readonly=False,)
     
-
-    name = fields.Char(string='Nombre', required=True, readonly=False,)
-	apell = fields.Char(string='Apellido', required=True, readonly=False,)
-	carr = fields.Char(string='Carrera', required=True, readonly=False,)
-	Horario = fields.Many2one('clase.horaria', string='Horario', required=True)
-    reclamos = fields.One2many('clase.sugerecla', 'unefa_id', string='Suge Recla', )
-
-   
-
-
-class claseCategoria(models.Model):
+   class claseCategoria(models.Model):
     _name = "clase.horario"
     
 
     name = fields.Char(string='Diurno', required=True, readonly=False,)
     noct = fields.Integer(string='Nocturno', required=True, readonly=False,)
     mixt = fields.Boolean(string='Mixto', required=True, readonly=False,)
-    
-    
-
-class claseunefa2(models.Model):
-    _name = "clase.sugerecla"
-    
-
-    name = fields.Char(string='Sugerencia', required=True, readonly=False,)
-    recla = fields.Char(string='Reclamos', required=True, readonly=False,)
-    
-    unefa_id = fields.Many2one('suge.recla', string='Unefa',)
-"""
+   
+    def create(self, vals):
+        Sugerencias = vals['suge']   
+        malas_palabras = ['culo','guebo','mamaguebo','maduro','mardito','trimardito']
+        lista_sugerencias = Sugerencias.split('')
+        for i in lista_sugerencias:
+			if i in malas_palabras:
+			    raise UserError(_('No se aceptan malas palabras.'))
+		 
+return super(SaleOrderLine, self).create(vals)
